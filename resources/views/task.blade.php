f5f20a2370d1cfadc94bdcf4eb59c4af93460c24
Miro Calope Lim <limmiro88@gmail.com>
2026-07-19 22:26:16 +0800

    v4

--- resources/views/task.blade.php
+++ resources/views/task.blade.php
@@ -8,30 +8,7 @@
 </head>
 <body>
     
-    <h1>Admin Panel</h1>
-
-    <form action="/admin" method="POST">
-        @csrf
-
-        <div>
-            <label for="name">Name</label>
-            <input type="text" id="name" name="name" placeholder="Enter name">
-        </div>
-
-        <div>
-            <label for="email">Email</label>
-            <input type="email" id="email" name="email" placeholder="Enter email">
-        </div>
-
-        <div>
-            <label for="message">Message</label>
-            <textarea id="message" name="message" rows="4" placeholder="Enter message"></textarea>
-        </div>
-
-        <div>
-            <button type="submit">Submit</button>
-        </div>
-    </form>
+    <h1>Display Task</h1>
 
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/task', function () {
-    return view('task');
-});
+Route::resource('/task', TaskController::class);
